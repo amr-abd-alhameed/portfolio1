5294121c0c2aff3dc69daa3f26d7fb35ddf7adc4
amr <aa5473@fayoum.edu.eg>
2023-02-07 00:10:14 +0300

    hi

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,4 @@
 import "./Home.scss";
-
 import circle from "../../../src/assets/images/circle-512.png";
 import man from "../../../src/assets/images/men-suit-png-9471.png";
 
@@ -7,22 +6,22 @@ import Text from "./component/Text";
 import Icons from "./component/Icons";
 
 const Home = () => {
-    return (
-        <>
-            <section className="home">
-                <div className="container">
-                    <div className="rings">
-                        <span className="one"></span>
-                        <img className="two" src={circle} alt="" />
-                        <img className="three" src={man} alt="" />
-                    </div>
-                    <Icons />
+  return (
+    <>
+      <section className="home">
+        <div className="container">
+          <div className="rings">
+            <span className="one"></span>
+            <img className="two" src={circle} alt="" />
+            <img className="three" src={man} alt="" />
+          </div>
+          <Icons />
 
-                    <Text />
-                </div>
-            </section>
-        </>
-    );
+          <Text />
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Home;
