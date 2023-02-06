08bac7cebc8445b877c22d4d4f07c2fa2292c422
amr <aa5473@fayoum.edu.eg>
2023-02-06 22:59:47 +0300

    setup gh-pages

--- src/components/Header/component/Item.js
+++ src/components/Header/component/Item.js
@@ -1,14 +1,16 @@
 import "./Item.scss";
 import React from "react";
+import { Link } from "react-router-dom";
 // import Link from "react-router-dom";
 
 const Item = (props) => {
-    return (
-        <li className="link">
-            <span className="span d-lg-none">{<props.icon />}</span>
-            <a href={`${props.hash}`}>{props.name}</a>
-        </li>
-    );
+  return (
+    <li className="link">
+      <span className="span d-lg-none">{<props.icon />}</span>
+      <Link to={props.hash}>{props.name}</Link>
+      {/* <a href={`${props.hash}`}></a> */}
+    </li>
+  );
 };
 
 export { Item };
